0d82a0337f2c2af8b16c4068bbf6b1bf78af0c45
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-01-26 16:17:38 +0100

    ended main task

--- js/script.js
+++ js/script.js
@@ -3,11 +3,31 @@ let age = prompt("Quanti hanni hai?");
 let priceForKm = 0.21;
 let finalPrice = priceForKm * km;
 
+let sales20 = ((finalPrice * 20) / 100) ;
+let sales40 =  ((finalPrice * 40) / 100) ;
+
+
+if (age <= 18){
+    
+document.getElementById("price").innerHTML = ( (finalPrice - sales20).toFixed(2) );
+    
+} else if ( age >= 65){
+    document.getElementById("price").innerHTML =  ( (finalPrice - sales40).toFixed(2) );
+
+
+} else{
+
+document.getElementById("price").innerHTML = ( finalPrice.toFixed(2) )}
 
 
 
 
 
-document.getElementById("price").innerHTML = finalPrice;
 
-console.log(finalPrice);
+
+console.log("sales 20% if age < 18:",sales20);
+
+console.log("sales 40% if age > 65:",sales40);
+console.log("price withouth sales:",finalPrice);
+
+
