9d9028fcbc1c7393bebbc171998a2c13ac77a3d3
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-01-26 15:23:40 +0100

    added .html base, added variable and first computaton

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let km = prompt("Quanti km devi percorrere?");
+let age = prompt("Quanti hanni hai?");
+let priceForKm = 0.21;
+let finalPrice = priceForKm * km;
+
+
+
+
+
+
+document.getElementById("price").innerHTML = finalPrice;
+
+console.log(finalPrice);
