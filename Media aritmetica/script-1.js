8e03c28e5cd24a822f05cde01d9af6b242aa8c14
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-01-29 14:44:28 +0100

    Media aritmentica added

---
+++ Media aritmetica/script-1.js
@@ -0,0 +1,15 @@
+
+
+
+
+let numberOne = Number(prompt("Scegli il primo numero"));
+let numberTwo = Number(prompt("Scegli il primo numero"));
+let numberTree = Number(prompt("Scegli il primo numero"));
+
+let sum = numberOne + numberTwo + numberTree;
+
+
+document.getElementById("arithmetic-average").innerHTML = sum / 3;
+
+
+console.log(sum);
